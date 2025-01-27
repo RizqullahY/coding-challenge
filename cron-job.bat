5f9ee7b79c9c964fd8a111983a8a853e1cbebd7c
Rizqullah Yusuf <rizqullahyusufrafly@gmail.com>
2025-01-27 15:43:06 +0700

    Automatic commit: 2025-01-27 03:43 PM

--- cron-job.bat
+++ cron-job.bat
@@ -3,12 +3,16 @@
 :: Get time now
 for /f "tokens=1-2 delims=:" %%a in ('time /T') do set time=%%a:%%b
 
-:: Show time on screen
-echo time now : %time%
+:: Get date
+for /f "tokens=2-4 delims=/ " %%a in ('date /t') do (
+  set year=%%c
+  set month=%%a
+  set day=%%b
+)
 
 :: Add to message commit
 git add .
-git commit -m "Automatic commit: $(date /t) %time%"
+git commit -m "Automatic commit: %year%-%month%-%day% %time%"
 git push origin master
 
 :: Close the cmd
