a30dbf12d72e27ffee6a3040914441c6b8e463dc
ARSYILA_RASYIDAH <rizqullahyusufrafly@gmail.com>
2025-03-15 10:54:55 +0700

    Automatic commit: 2025-03-15 10:54

---
+++ CODEWARS/Kata Training/7 Kyu/exesAndOhs.py
@@ -0,0 +1,17 @@
+# https://www.codewars.com/kata/55908aad6620c066bc00002a/train/python
+
+def xo(s):
+    s = list(s.lower())
+    # print(s)
+    count_x = s.count('x')
+    count_o = s.count('o')
+    # print(count_o)
+    return count_o == count_x
+
+print(xo("xxoox"))  # False
+print(xo("ooxx"))   # True
+print(xo("xooxx"))  # False
+print(xo("ooxXm"))  # True
+print(xo("zpzpzpp")) # True
+print(xo("zzoo"))   # False
+print(xo("oxOx"))   # True
