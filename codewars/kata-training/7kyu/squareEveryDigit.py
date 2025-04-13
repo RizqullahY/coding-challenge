01f47dc59aa7af8c0f7580b9b093263bad47ac44
Rafly Rizqullah Yusuf <rizqullahyusufrafly@gmail.com>
2025-04-13 11:38:40 +0700

    Automatic commit: 2025-04-13 11:38

--- codewars/kata-training/7kyu/squareEveryDigit.py
+++ codewars/kata-training/7kyu/squareEveryDigit.py
@@ -1,4 +1,13 @@
 # https://www.codewars.com/kata/546e2562b03326a88e000020/train/python
 
 def square_digits(num):
-    pass
+    result = []
+    for i in str(num):
+        s = int(i) ** 2
+        result.append(str(s))
+    x = ''.join(result)
+    return int(x)
+
+print(square_digits(9119))
+print(square_digits(456))
+print(square_digits(0))
